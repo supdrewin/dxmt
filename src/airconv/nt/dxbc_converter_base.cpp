ffd907fcea290b9c440fc1d0cf5c4fa0cfb8f57c
Feifan He <feifan@codeweavers.com>
2026-05-11 15:14:59 +0800

    fix(airconv): correct `firstbit_shi` implementation for negative integer
    Signed-off-by: Feifan He <feifan@codeweavers.com>

--- src/airconv/nt/dxbc_converter_base.cpp
+++ src/airconv/nt/dxbc_converter_base.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2026 Feifan He for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 #include "dxbc_converter_base.hpp"
 #include "../dxbc_converter.hpp"
 #include "air_builder.hpp"
@@ -1046,7 +1064,8 @@ Converter::operator()(const InstIntegerUnaryOp &unary) {
     break;
   case IntegerUnaryOp::FirstHiBitSigned:
     Value = ir.CreateSelect(
-        ir.CreateIsNotNeg(Value), air.CreateCountZero(Value, false), air.CreateCountZero(ir.CreateNot(Value), true)
+        ir.CreateIsNotNeg(Value), air.CreateCountZero(Value, false),
+        ir.CreateAdd(air.CreateCountZero(ir.CreateShl(ir.CreateNot(Value), ir.getInt32(1)), false), ir.getInt32(1))
     );
     Value = MaxIfInMask(~((uint32_t)0x1f), Value);
     break;
